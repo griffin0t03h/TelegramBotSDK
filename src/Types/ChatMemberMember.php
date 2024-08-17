92ddff500ee8bad8405a95ad7e2688be671b017e
Sina Moradi <sinamorady101@gmail.com>
2024-08-18 00:19:44 +0330

    Added the field until_date to the class ChatMemberMember

--- src/Types/ChatMemberMember.php
+++ src/Types/ChatMemberMember.php
@@ -10,6 +10,31 @@ namespace TelegramBotSDK\Types;
  */
 class ChatMemberMember extends ChatMember
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @var array
+     */
+    protected static array $requiredParams = ['status', 'user'];
+
+    /**
+     * {@inheritdoc}
+     *
+     * @var array
+     */
+    protected static array $map = [
+        'status' => true,
+        'user' => User::class,
+        'until_date' => true,
+    ];
+
+    /**
+     * Optional. Date when the user's subscription will expire; Unix time
+     *
+     * @var int
+     */
+    protected int $untilDate;
+
     public static function fromResponse($data): ChatMemberMember|static
     {
         self::validate($data);
@@ -18,5 +43,22 @@ class ChatMemberMember extends ChatMember
         $instance->map($data);
 
         return $instance;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUntilDate(): int
+    {
+        return $this->untilDate;
+    }
+
+    /**
+     * @param int $untilDate
+     * @return void
+     */
+    public function setUntilDate(int $untilDate): void
+    {
+        $this->untilDate = $untilDate;
     }
 }
